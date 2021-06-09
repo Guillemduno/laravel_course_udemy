78edb802efdcea82dcbb78b722b9f58217a72720
guillemduno <guillemduno@gmail.com>
2021-06-09 23:09:26 +0200

    Post controller and Route resource added

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,21 +3,18 @@
 @section('title', 'Blog post')
 
 @section('content')
-{{-- @if (count($posts))
-    @foreach ($posts as $key => $post)
-    <h1>{{ $key }} . {{ $post['title'] }}</h1>
-    <p>{{ $post['content'] }}</p> 
-    @endforeach
-@else
-    <p>No posts found!</p>
-@endif --}}
+  {{-- @if (count($posts))
+      @foreach ($posts as $key => $post)
+      <h1>{{ $key }} . {{ $post['title'] }}</h1>
+      <p>{{ $post['content'] }}</p> 
+      @endforeach
+  @else
+      <p>No posts found!</p>
+  @endif --}}
 
-@forelse ($posts as $key => $post)
-@include('posts.partials.post')
-
-@empty
-    <p>No posts found!</p>
-@endforelse
-  
- 
+  @forelse ($posts as $key => $post)
+    @include('posts.partials.post')
+  @empty
+      <p>No posts found!</p>
+  @endforelse
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
 use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
+use Iluminate\Http\Request;
+use PhpParser\Node\Expr\FuncCall;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,88 +27,134 @@ use Illuminate\Support\Facades\Route;
 //     return 'Contact';
 // })->name('home.contact');
 
-Route::view('/', 'home.index')->name('home.index');
-Route::view('/contact', 'home.contact')->name('home.contact');
+Route::get('/', [HomeController::class, 'home'])->name('home.index');
+Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact');
+Route::get('/single', AboutController::class);
 
-$posts = [
-        1 => [
-            'title' => 'Intro to Laravel',
-            'content' => 'This is a short intro to Laravel',
-            'is_new' => true,
-            'has_comments' => true
-        ],
-        2 => [
-            'title' => 'Intro to PHP',
-            'content' => 'This is a short intro to PHP',
-            'is_new' => false
-        ],
-        3 => [
-            'title' => 'Intro to Javascript',
-            'content' => 'This is a short intro to Javascript',
-            'is_new' => false
-        ]
-];
+// $posts = [
+//         1 => [
+//             'title' => 'Intro to Laravel',
+//             'content' => 'This is a short intro to Laravel',
+//             'is_new' => true,
+//             'has_comments' => true
+//         ],
+//         2 => [
+//             'title' => 'Intro to PHP',
+//             'content' => 'This is a short intro to PHP',
+//             'is_new' => false
+//         ],
+//         3 => [
+//             'title' => 'Intro to Javascript',
+//             'content' => 'This is a short intro to Javascript',
+//             'is_new' => false
+//         ]
+// ];
 
-Route::get('/posts', function () use ($posts) {
-    // compact($post) it's the same ['post' => $posts]
-    return view('posts.index', ['posts' => $posts]);
-});
+// Route::get('/posts', function () use ($posts) {
+//     // compact($post) it's the same ['post' => $posts]
+//     return view('posts.index', ['posts' => $posts]);
+// });
 
-Route::get('posts/{id}', function ($id) use ($posts) {
-    abort_if(!isset($posts[$id]), 404);
-    // return 'Blog post '.$id;
-    return view('posts.show', ['post' => $posts[$id]]);
-})
-// ->where([
-//     'id' => '[0-9]+'x
-//     ])
-->name('posts.show');
+// Route::get('/posts', function () use ($posts) {
+//     // dd(request()->all()); // dd() is a shortcut for dump and die.
+//     dd((int)request()->input('page', 1)); // For query parameters, Data center, forms, json.
+//     // dd((int)request()->query('page', 1)); // Specific for query parameters 
+//     // compact($post) it's the same ['post' => $posts]
+//     return view('posts.index', ['posts' => $posts]);
+// });
+
+
+// Route::resource('posts', PostController::class);
+Route::resource('/posts', PostController::class)->only(['index', 'show']);
+
+// Route::get('/posts/{id}', function ($id) use ($posts) {
+//     abort_if(!isset($posts[$id]), 404);
+//     // return 'Blog post '.$id;
+//     return view('posts.show', ['post' => $posts[$id]]);
+// })
+// // ->where([
+// //     'id' => '[0-9]+'x
+// //     ])
+// ->name('posts.show');
 
 Route::get('recent-posts/{days_ago?}', function ($days_ago = 20) {
     return 'Recent post form '.$days_ago.' days ago';
 })->name('post.recent.index');
+// ->middleware('auth');
  
 
-// Grouping Routes
-Route::prefix('/fun')->name('fun.')->group(function() use($posts){
+// // Grouping Routes
+// Route::prefix('/fun')->name('fun.')->group(function() use($posts){
 
-    //  Responses, Codes, Headers, and Cookies
-    Route::get('/responses', function () use($posts) {
-      return response($posts, 201)
-        ->header('Content-Type', 'application/json')
-        ->cookie('MY_COOKIE', 'Guillem Duñó', 3600);
-    })->name('responses');
+//     //  Responses, Codes, Headers, and Cookies
+//     Route::get('/responses', function () use($posts) {
+//       return response($posts, 201)
+//         ->header('Content-Type', 'application/json')
+//         ->cookie('MY_COOKIE', 'Guillem Duñó', 3600);
+//     })->name('responses');
   
-    // Redirect
-    Route::get('/redirect', function () {
-        return redirect('/contact');
-    })->name('redirect');
+//     // Redirect
+//     Route::get('/redirect', function () {
+//         return redirect('/contact');
+//     })->name('redirect');
   
-    // Back
-    Route::get('/back', function () {
-        return back();
-    })->name('back');
+//     // Back
+//     Route::get('/back', function () {
+//         return back();
+//     })->name('back');
   
-    // Name route
-    Route::get('/route_name', function () {
-        return redirect()->route('posts.show', ['id' => 1]);
-    })->name('route_name');
+//     // Name route
+//     Route::get('/route_name', function () {
+//         return redirect()->route('posts.show', ['id' => 1]);
+//     })->name('route_name');
   
-    // Away
-    Route::get('/away', function () {
-        return redirect()->away('https://google.com');
-    })->name('away');
+//     // Away
+//     Route::get('/away', function () {
+//         return redirect()->away('https://google.com');
+//     })->name('away');
   
-    // Return a json
-    Route::get('/json', function () use ($posts) {
-        return response()->json($posts);
-    })->name('json');
+//     // Return a json
+//     Route::get('/json', function () use ($posts) {
+//         return response()->json($posts);
+//     })->name('json');
   
-    // Download a file, and renamed.
-    Route::get('/download', function () {
-        return response()->download(public_path('/daniel.jpg'), 'face.jpg');
-    })->name('download');
-});
+//     // Download a file, and renamed.
+//     Route::get('/download', function () {
+//         return response()->download(public_path('/daniel.jpg'), 'face.jpg');
+//     })->name('download');
+// });
 
+// Request Input
+// $archived = $request->boolean('archived'); // Returns true, 'true', 1, '1', 'on', 'yes' or false.
+// $input = $request->only(['username', 'password']);
+// $input = $request->only('username', 'password');
+// $input = $request->except(['credit_card']);
+// $input = $request->except('credit_card');
 
+// if ($request->has('name')) {
+//   # code...
+// }
+// if ($request->has(['name', 'email'])) {
+//   # code...
+// }
 
+// $request->whenHas('name', function($input){
+//   //
+// });
+
+// // The opposite method of whenHas i missing
+// $request->missing('name', function($input){
+//   //
+// });
+
+// if ($request->hasAny(['name', 'email'])) {
+//   # code...
+// }
+
+// if ($request->filled('name')) {
+//   // Return true if value is present
+// }
+
+// $request->whenFilled('name', function($input){
+//   //
+// });
