66a4e233f29178057710ec95cb56da17e3020ff9
Guillem Duñó <guillemduno@gmail.com>
2022-03-18 23:13:39 +0100

    another commit

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -38,4 +38,37 @@ class PostTest extends TestCase
                ['title' =>'La Cobra']
         );
     }
+
+
+    public function testStoreValid(){
+        $params = [
+            'title' => 'Valid title',
+            'content' => 'At least 10 characters' 
+        ];
+
+        $this->post('/posts', $params)
+            ->assertStatus(302)
+            ->assertSessionHas('status');
+        
+        $this->assertEquals(session('status'), 'The blog post was created');
+    }
+
+    public function testStoreFail(){
+
+        $params = [
+            'title' => 'x',
+            'content' => 'x'
+        ]; 
+
+        $this->post('/posts', $params)
+        ->assertStatus(302)
+        ->assertSessionHas('errors');
+
+        $messages = session('errors')->getMessages();
+
+        $this->assertEquals($messages['title'][0], 'The title must be at least 5 characters.');
+        $this->assertEquals($messages['content'][0], 'The content must be at least 10 characters.');
+
+        // dd($messages->getMessages());
+    }
 }
