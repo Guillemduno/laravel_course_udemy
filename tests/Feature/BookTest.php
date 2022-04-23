de92294a2c7e6e659a7713f6266ff603805f8820
Guillem Duñó <guillemduno@gmail.com>
2022-04-23 09:09:37 +0200

    test update and delete added

--- tests/Feature/BookTest.php
+++ tests/Feature/BookTest.php
@@ -79,10 +79,67 @@ class BookTest extends TestCase
                 ->assertSessionHas('errors');
         
         $messages = session('errors')->getMessages();
-
+        // dd($messages);
         $this->assertEquals($messages['title'][0], 'The title must be at least 5 characters.');
         $this->assertEquals($messages['year'][0], 'The year field is required.');
         $this->assertEquals($messages['pages'][0], 'The pages field is required.');
-        // dd($messages->getMessages());
+    }
+
+    public function testBookUpdate(){
+
+        $book = new Book();
+        $book->title = 'Contigo hasta el final2';
+        $book->year = 2004;
+        $book->pages = 396;
+        $book->save();
+
+        $this->assertDatabaseHas('books', [
+            'title' => 'Contigo hasta el final2',
+            'year' => 2004,
+            'pages' => 396
+        ]);
+
+        $params = [
+            'title' => 'Nunca contigo 2',
+            'year' => 2029,
+            'pages' => 143
+        ];
+
+        $this->put("/books/{$book->id}", $params)
+            ->assertStatus(302)
+            ->assertSessionHas('status');
+
+        $this->assertEquals(session('status'), "The book 'Nunca contigo 2' was edited");
+
+        $this->assertDatabaseMissing('books', $book->toArray());
+        $this->assertDatabaseHas('books', [
+            'title' => 'Nunca contigo 2',
+            'year' => 2029,
+            'pages' => 143
+        ]);
+
+    }
+
+    public function testDeleteBook(){
+
+        $book           = new Book();
+        $book->title    = 'Contigo hasta el final2';
+        $book->year     = 2004;
+        $book->pages    = 396;
+        $book->save();
+
+        $this->assertDatabaseHas('books', [
+            'title'     => 'Contigo hasta el final2',
+            'year'      => 2004,
+            'pages'     => 396
+        ]);
+
+        $this->delete("books/{$book->id}", )
+            ->assertStatus(302)
+            ->assertSessionHas('status');
+
+        $this->assertEquals(session('status'), "The book 'Contigo hasta el final2' was deleted!");
+
+        $this->assertDatabaseMissing('books', $book->toArray());
     }
 }
