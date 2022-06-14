6356d1181d2bf5a682ca9c78f92795d0eb428915
Guillem Duñó <guillemduno@gmail.com>
2022-06-14 06:25:48 +0200

    register form errors added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,46 +7,37 @@
         <div class="form-group">
             <label for="name">Name</label>
             <input type="name" name="name" id="name" value="{{old('name')}}" required class="form-control {{$errors->has('name')?' is-invalid':''}}">
+            @if($errors->has('name'))
+                <span class="invalid-feedback">
+                    {{ $errors->first('name')}}
+                </span>
+            @endif
         </div>
-
-        @if($errors->has('name'))
-            <span class="invalid-feedback">
-                {{ $errors->first('name')}}
-            </span>
-        @endif
 
         <div class="form-group">
             <label for="email">Email</label>
-            <input type="email" name="email" id="email" value="{{old('email')}}" required class="form-control">
+            <input type="email" name="email" id="email" value="{{old('email')}}" required class="form-control {{$errors->has('email')?' is-invalid':''}}">
+            @if($errors->has('email'))
+                <span class="invalid-feedback">
+                    {{$errors->first('email')}}
+                </span>  
+            @endif
         </div>
-
-        @if ($errors->has('email'))
-            <span class="invalid-feedback">
-                {{ $errors->first('email')}}
-            </span>
-        @endif
 
         <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" name="password" id="password" required class="form-control">
+            <input type="password" name="password" id="password" required class="form-control {{$errors->has('password')? 'is-invalid':''}}">
+            @if ($errors->has('password'))
+                <span class="invalid-feedback">
+                    {{ $errors->first('password')}}
+                </span>
+            @endif
         </div>
-
-        @if ($errors->has('password'))
-            <span class="invalid-feedback">
-                {{ $errors->first('password')}}
-            </span>
-        @endif
 
         <div class="form-group">
-            <label for="retyped">Retyped Password</label>
-            <input type="password" name="retyped" id="retyped" required class="form-control">
+            <label for="password_confirmation">Retyped Password</label>
+            <input type="password_confirmation" name="password_confirmation" id="password_confirmation" required class="form-control">
         </div>
-
-        @if ($errors->has('retyped'))
-            <span class="invalid-feedback">
-                {{ $errors->first('retyped')}}
-            </span>
-        @endif
 
         <input type="submit" value="Register" class="btn btn-primary btn-block">
         
